b8192339b0e6d03018c9e4bb7f0e4bc5af4d6b9b
Ashwani K Goel <intecons@users.noreply.github.com>
2015-10-09 09:39:24 +0530

    Update carmon-plugin.js

--- assets/javascripts/carmon-plugin.js
+++ assets/javascripts/carmon-plugin.js
@@ -1,5 +1,4 @@
 (function() {
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(http?:)?\/\/www\.intecons\.in\/.+/i);
 })();
 
